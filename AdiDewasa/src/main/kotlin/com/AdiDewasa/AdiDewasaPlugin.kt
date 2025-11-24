72926b32e59d0be7f90e76be5b86149dfa91ffd5
michat88 <gokilmichat@gmail.com>
2025-11-25 01:14:21 +0900

    Update AdiDewasaPlugin.kt

--- AdiDewasa/src/main/kotlin/com/AdiDewasa/AdiDewasaPlugin.kt
+++ AdiDewasa/src/main/kotlin/com/AdiDewasa/AdiDewasaPlugin.kt
@@ -6,10 +6,6 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 @CloudstreamPlugin
 class AdiDewasaPlugin: BasePlugin() {
     override fun load() {
-        // 1. Daftarkan Penyedia Film (AdiDewasa)
         registerMainAPI(AdiDewasa())
-        
-        // 2. Daftarkan Penyedia Subtitle Tambahan (MySubSource)
-        registerSubtitleProvider(MySubSource())
     }
 }
