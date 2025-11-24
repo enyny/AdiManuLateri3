39ee1aa081b511866cea39b7630e03ec6e69b60c
michat88 <gokilmichat@gmail.com>
2025-11-25 01:09:23 +0900

    Update AdiDewasaPlugin.kt

--- AdiDewasa/src/main/kotlin/com/AdiDewasa/AdiDewasaPlugin.kt
+++ AdiDewasa/src/main/kotlin/com/AdiDewasa/AdiDewasaPlugin.kt
@@ -1,12 +1,15 @@
-package com.AdiDewasa // Diubah dari com.dramafull
+package com.AdiDewasa
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
-class AdiDewasaPlugin: BasePlugin() { // Diubah dari DramaFullPlugin
+class AdiDewasaPlugin: BasePlugin() {
     override fun load() {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(AdiDewasa()) // Diubah dari DramaFull()
+        // 1. Daftarkan Penyedia Film (AdiDewasa)
+        registerMainAPI(AdiDewasa())
+        
+        // 2. Daftarkan Penyedia Subtitle Tambahan (MySubSource)
+        registerSubtitleProvider(MySubSource())
     }
 }
