667a90fb5965de0f11c12f44fa38d5d758fd5db9
michat88 <gokilmichat@gmail.com>
2025-11-30 12:43:43 +0900

    Update build.gradle.kts

--- Adimoviebox/build.gradle.kts
+++ Adimoviebox/build.gradle.kts
@@ -5,7 +5,7 @@ version = 1
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "AdiManuLateri3" // Deskripsi yang diperbarui
+    description = "Nonton sampe biji mata kaluar" // Deskripsi yang diperbarui
     language    = "en" // Bahasa dari Moviebox
     authors = listOf("AdimovieboxUser") // Ganti sesuai keinginan Anda
 
