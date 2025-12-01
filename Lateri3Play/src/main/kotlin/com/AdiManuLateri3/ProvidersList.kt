403fcce5760ba2efd551a0fc523e9a9c65791734
michat88 <gokilmichat@gmail.com>
2025-12-02 06:10:12 +0900

    Update ProvidersList.kt

--- Lateri3Play/src/main/kotlin/com/AdiManuLateri3/ProvidersList.kt
+++ Lateri3Play/src/main/kotlin/com/AdiManuLateri3/ProvidersList.kt
@@ -87,7 +87,7 @@ fun buildProviders(): List<Provider> {
             invokeMovieBox(res.title, res.season, res.episode, subtitleCallback, callback)
         },
         Provider("vidfast", "VidFast") { res, subtitleCallback, callback, _, _ ->
-            // FIX: Menambahkan subtitleCallback yang sebelumnya hilang
+            // FIX: Menambahkan subtitleCallback
             invokeVidFast(res.id, res.season, res.episode, callback, subtitleCallback)
         },
         Provider("dramadrip", "DramaDrip") { res, subtitleCallback, callback, _, _ ->
