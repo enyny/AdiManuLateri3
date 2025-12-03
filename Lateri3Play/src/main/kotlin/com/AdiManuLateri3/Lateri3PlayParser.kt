236b882c1b576034dff0c07632141a847ad9bd64
michat88 <gokilmichat@gmail.com>
2025-12-03 19:02:45 +0900

    Update Lateri3PlayParser.kt

--- Lateri3Play/src/main/kotlin/com/AdiManuLateri3/Lateri3PlayParser.kt
+++ Lateri3Play/src/main/kotlin/com/AdiManuLateri3/Lateri3PlayParser.kt
@@ -1,224 +1,149 @@
 package com.AdiManuLateri3
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.google.gson.annotations.SerializedName
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.utils.ExtractorLink
 
-// ==================== TMDB & MAIN STRUCTURES ====================
+// Import fungsi-fungsi dari Extractor
+import com.AdiManuLateri3.Lateri3PlayExtractor.invokeUhdmovies
+import com.AdiManuLateri3.Lateri3PlayExtractor.invokeVegamovies
+import com.AdiManuLateri3.Lateri3PlayExtractor.invokeMoviesmod
+import com.AdiManuLateri3.Lateri3PlayExtractor.invokeMultimovies
+import com.AdiManuLateri3.Lateri3PlayExtractor.invokeRidomovies
+import com.AdiManuLateri3.Lateri3PlayExtractor.invokeMoviesdrive
+import com.AdiManuLateri3.Lateri3PlayExtractor.invokeDotmovies
+import com.AdiManuLateri3.Lateri3PlayExtractor.invokeRogmovies
+import com.AdiManuLateri3.Lateri3PlayExtractor.invokeHdmovie2
+import com.AdiManuLateri3.Lateri3PlayExtractor.invokeTopMovies
+import com.AdiManuLateri3.Lateri3PlayExtractor.invokeBollyflix
 
-data class LinkData(
-    val id: Int? = null,
-    val imdbId: String? = null,
-    val type: String? = null,
-    val season: Int? = null,
-    val episode: Int? = null,
-    val title: String? = null,
-    val year: Int? = null,
-    val orgTitle: String? = null,
-    val epsTitle: String? = null,
-    val date: String? = null,
-)
-
-data class Data(
-    val id: Int? = null,
-    val type: String? = null,
-)
-
-data class Results(
-    @JsonProperty("results") val results: ArrayList<Media>? = arrayListOf(),
-)
-
-data class Media(
-    @JsonProperty("id") val id: Int? = null,
-    @JsonProperty("name") val name: String? = null,
-    @JsonProperty("title") val title: String? = null,
-    @JsonProperty("original_title") val originalTitle: String? = null,
-    @JsonProperty("original_name") val originalName: String? = null,
-    @JsonProperty("media_type") val mediaType: String? = null,
-    @JsonProperty("poster_path") val posterPath: String? = null,
-    @JsonProperty("vote_average") val voteAverage: Double? = null,
-)
-
-data class MediaDetail(
-    @JsonProperty("id") val id: Int? = null,
-    @JsonProperty("title") val title: String? = null,
-    @JsonProperty("name") val name: String? = null,
-    @JsonProperty("original_title") val originalTitle: String? = null,
-    @JsonProperty("original_name") val originalName: String? = null,
-    @JsonProperty("poster_path") val posterPath: String? = null,
-    @JsonProperty("backdrop_path") val backdropPath: String? = null,
-    @JsonProperty("release_date") val releaseDate: String? = null,
-    @JsonProperty("first_air_date") val firstAirDate: String? = null,
-    @JsonProperty("overview") val overview: String? = null,
-    @JsonProperty("runtime") val runtime: Int? = null,
-    @JsonProperty("vote_average") val vote_average: Any? = null,
-    @JsonProperty("status") val status: String? = null,
-    @JsonProperty("genres") val genres: ArrayList<Genres>? = arrayListOf(),
-    @JsonProperty("keywords") val keywords: KeywordResults? = null,
-    @JsonProperty("last_episode_to_air") val last_episode_to_air: LastEpisodeToAir? = null,
-    @JsonProperty("seasons") val seasons: ArrayList<Seasons>? = arrayListOf(),
-    @JsonProperty("videos") val videos: ResultsTrailer? = null,
-    @JsonProperty("external_ids") val external_ids: ExternalIds? = null,
-    @JsonProperty("credits") val credits: Credits? = null,
-    @JsonProperty("recommendations") val recommendations: ResultsRecommendations? = null,
-)
-
-data class Genres(
-    @JsonProperty("name") val name: String? = null,
-)
-
-data class KeywordResults(
-    @JsonProperty("results") val results: ArrayList<Keywords>? = arrayListOf(),
-    @JsonProperty("keywords") val keywords: ArrayList<Keywords>? = arrayListOf(),
-)
-
-data class Keywords(
-    @JsonProperty("name") val name: String? = null,
-)
-
-data class Seasons(
-    @JsonProperty("season_number") val seasonNumber: Int? = null,
-    @JsonProperty("air_date") val airDate: String? = null,
-)
-
-data class MediaDetailEpisodes(
-    @JsonProperty("episodes") val episodes: ArrayList<Episodes>? = arrayListOf(),
-)
-
-data class Episodes(
-    @JsonProperty("name") val name: String? = null,
-    @JsonProperty("overview") val overview: String? = null,
-    @JsonProperty("air_date") val airDate: String? = null,
-    @JsonProperty("still_path") val stillPath: String? = null,
-    @JsonProperty("vote_average") val voteAverage: Double? = null,
-    @JsonProperty("episode_number") val episodeNumber: Int? = null,
-    @JsonProperty("season_number") val seasonNumber: Int? = null,
-)
-
-data class Credits(
-    @JsonProperty("cast") val cast: ArrayList<Cast>? = arrayListOf(),
-)
-
-data class Cast(
-    @JsonProperty("name") val name: String? = null,
-    @JsonProperty("original_name") val originalName: String? = null,
-    @JsonProperty("character") val character: String? = null,
-    @JsonProperty("profile_path") val profilePath: String? = null,
-)
-
-data class ResultsTrailer(
-    @JsonProperty("results") val results: ArrayList<Trailers>? = arrayListOf(),
-)
-
-data class Trailers(
-    @JsonProperty("key") val key: String? = null,
-    @JsonProperty("type") val type: String? = null,
-)
-
-data class ResultsRecommendations(
-    @JsonProperty("results") val results: ArrayList<Media>? = arrayListOf(),
-)
-
-data class ExternalIds(
-    @JsonProperty("imdb_id") val imdb_id: String? = null,
-)
-
-data class LastEpisodeToAir(
-    @JsonProperty("season_number") val season_number: Int? = null,
-)
-
-data class TmdbDate(
-    val today: String,
-    val nextWeek: String,
-    val lastWeekStart: String
-)
-
-// ==================== CONFIGURATION & DOMAINS ====================
-
-data class DomainsParser(
-    val moviesdrive: String,
-    @JsonProperty("HDHUB4u")
-    val hdhub4u: String,
-    @JsonProperty("4khdhub")
-    val n4khdhub: String,
-    @JsonProperty("MultiMovies")
-    val multiMovies: String,
-    val bollyflix: String,
-    @JsonProperty("UHDMovies")
-    val uhdmovies: String,
-    val moviesmod: String,
-    val topMovies: String,
-    val hdmovie2: String,
-    val vegamovies: String,
-    val rogmovies: String,
-    val luxmovies: String,
-    val xprime: String,
-    val extramovies: String,
-    val dramadrip: String,
-    val toonstream: String,
-)
-
-// ==================== COMMON RESPONSES ====================
-
-data class ResponseHash(
-    @JsonProperty("embed_url") val embed_url: String,
-    @JsonProperty("key") val key: String? = null,
-    @JsonProperty("type") val type: String? = null,
-)
-
-// ==================== SUBTITLES ====================
-
-data class SubtitlesAPI(
-    val subtitles: List<Subtitle>,
-    val cacheMaxAge: Long,
-)
-
-data class Subtitle(
+// Definisi Data Class Provider
+// PERBAIKAN: Menggunakan LinkData langsung (bukan Lateri3Play.LinkData)
+data class Provider(
     val id: String,
-    val url: String,
-    @JsonProperty("SubEncoding")
-    val subEncoding: String,
-    val lang: String,
-    val m: String,
-    val g: String,
+    val name: String,
+    val invoke: suspend (
+        res: LinkData, 
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) -> Unit
 )
 
-data class WyZIESUB(
-    val id: String,
-    val url: String,
-    val flagUrl: String,
-    val format: String,
-    val display: String,
-    val language: String,
-    val media: String,
-    val isHearingImpaired: Boolean,
-)
-
-// ==================== PROVIDER SPECIFIC PARSERS ====================
-
-// --- RidoMovies ---
-data class RidoSearch(
-    @JsonProperty("data") var data: RidoData? = null,
-)
-
-data class RidoData(
-    @JsonProperty("url") var url: String? = null,
-    @JsonProperty("items") var items: ArrayList<RidoItems>? = arrayListOf(),
-)
-
-data class RidoItems(
-    @JsonProperty("slug") var slug: String? = null,
-    @JsonProperty("contentable") var contentable: RidoContentable? = null,
-)
-
-data class RidoContentable(
-    @JsonProperty("imdbId") var imdbId: String? = null,
-    @JsonProperty("tmdbId") var tmdbId: Int? = null,
-)
-
-data class RidoResponses(
-    @JsonProperty("data") var data: ArrayList<RidoDataUrl>? = arrayListOf(),
-)
-
-data class RidoDataUrl(
-    @JsonProperty("url") var url: String? = null,
-)
+@RequiresApi(Build.VERSION_CODES.O)
+fun buildProviders(): List<Provider> {
+    return listOf(
+        Provider("uhdmovies", "UHD Movies") { res, sub, cb ->
+            invokeUhdmovies(
+                title = res.title,
+                year = res.year,
+                season = res.season,
+                episode = res.episode,
+                callback = cb,
+                subtitleCallback = sub
+            )
+        },
+        Provider("vegamovies", "VegaMovies") { res, sub, cb ->
+            invokeVegamovies(
+                title = res.title,
+                year = res.year,
+                season = res.season,
+                episode = res.episode,
+                imdbId = res.imdbId,
+                subtitleCallback = sub,
+                callback = cb
+            )
+        },
+        Provider("moviesmod", "MoviesMod") { res, sub, cb ->
+            invokeMoviesmod(
+                imdbId = res.imdbId,
+                year = res.year,
+                season = res.season,
+                episode = res.episode,
+                subtitleCallback = sub,
+                callback = cb
+            )
+        },
+        Provider("multimovies", "MultiMovies") { res, sub, cb ->
+            invokeMultimovies(
+                title = res.title,
+                season = res.season,
+                episode = res.episode,
+                subtitleCallback = sub,
+                callback = cb
+            )
+        },
+        Provider("ridomovies", "RidoMovies") { res, sub, cb ->
+            invokeRidomovies(
+                tmdbId = res.id,
+                imdbId = res.imdbId,
+                season = res.season,
+                episode = res.episode,
+                subtitleCallback = sub,
+                callback = cb
+            )
+        },
+        Provider("moviesdrive", "MoviesDrive") { res, sub, cb ->
+            invokeMoviesdrive(
+                title = res.title,
+                season = res.season,
+                episode = res.episode,
+                year = res.year,
+                imdbId = res.imdbId,
+                subtitleCallback = sub,
+                callback = cb
+            )
+        },
+        Provider("dotmovies", "DotMovies") { res, sub, cb ->
+            invokeDotmovies(
+                imdbId = res.imdbId,
+                title = res.title,
+                year = res.year,
+                season = res.season,
+                episode = res.episode,
+                sub = sub,
+                cb = cb
+            )
+        },
+        Provider("rogmovies", "RogMovies") { res, sub, cb ->
+            invokeRogmovies(
+                imdbId = res.imdbId,
+                title = res.title,
+                year = res.year,
+                season = res.season,
+                episode = res.episode,
+                sub = sub,
+                cb = cb
+            )
+        },
+        Provider("hdmovie2", "HDMovie2") { res, sub, cb ->
+            invokeHdmovie2(
+                title = res.title,
+                year = res.year,
+                season = res.season,
+                episode = res.episode,
+                subtitleCallback = sub,
+                callback = cb
+            )
+        },
+        Provider("topmovies", "TopMovies") { res, sub, cb ->
+            invokeTopMovies(
+                imdbId = res.imdbId,
+                year = res.year,
+                season = res.season,
+                episode = res.episode,
+                subtitleCallback = sub,
+                callback = cb
+            )
+        },
+        Provider("bollyflix", "BollyFlix") { res, sub, cb ->
+            invokeBollyflix(
+                id = res.imdbId,
+                season = res.season,
+                episode = res.episode,
+                subtitleCallback = sub,
+                callback = cb
+            )
+        }
+    )
+}
