e82bbf7caa587eabb9f15b8a8cf17c8233345433
michat88 <gokilmichat@gmail.com>
2025-12-03 09:12:15 +0900

    Update Lateri3PlayPlugin.kt

--- Lateri3Play/src/main/kotlin/com/AdiManuLateri3/Lateri3PlayPlugin.kt
+++ Lateri3Play/src/main/kotlin/com/AdiManuLateri3/Lateri3PlayPlugin.kt
@@ -1,33 +1,45 @@
 package com.AdiManuLateri3
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.api.Log
+import com.lagradost.cloudstream3.extractors.DoodYtExtractor
+import com.lagradost.cloudstream3.extractors.FileMoon
+import com.lagradost.cloudstream3.extractors.MixDrop
+import com.lagradost.cloudstream3.extractors.Mp4Upload
+import com.lagradost.cloudstream3.extractors.OkRu
+import com.lagradost.cloudstream3.extractors.StreamSB
+import com.lagradost.cloudstream3.extractors.StreamTape
+import com.lagradost.cloudstream3.extractors.StreamWishExtractor
+import com.lagradost.cloudstream3.extractors.VidHidePro
+import com.lagradost.cloudstream3.extractors.Voe
 
 @CloudstreamPlugin
 class Lateri3PlayPlugin: Plugin() {
-
     override fun load(context: Context) {
-        val sharedPref = context.getSharedPreferences("Lateri3Play", Context.MODE_PRIVATE)
+        // Mendaftarkan API Utama
+        registerMainAPI(Lateri3Play())
 
-        // HANYA daftarkan MainAPI yang diperlukan (Lateri3Play)
-        registerMainAPI(Lateri3Play(sharedPref))
-        Log.d("Lateri3Play", "Registered plugin: Lateri3Play")
-
-        // Hapus SEMUA extractor pihak ketiga (hanya menggunakan yang disederhanakan)
-        // Extractors yang digunakan akan diimplementasikan secara langsung di StreamPlayExtractor/StreamPlayUtils
-        
-        // Buka menu pengaturan
-        openSettings = { ctx ->
-            val act = ctx as AppCompatActivity
-            if (!act.isFinishing && !act.isDestroyed) {
-                val frag = MainSettingsFragment(this, sharedPref)
-                frag.show(act.supportFragmentManager, "Frag")
-            } else {
-                Log.e("Plugin", "Activity is not valid anymore, cannot show settings dialog")
-            }
-        }
+        // Mendaftarkan 10 Extractor Pilihan (Tanpa Secret Keys rumit)
+        // 1. StreamWish
+        registerExtractorAPI(StreamWishExtractor())
+        // 2. FileMoon
+        registerExtractorAPI(FileMoon())
+        // 3. Mp4Upload
+        registerExtractorAPI(Mp4Upload())
+        // 4. DoodStream / DoodYT
+        registerExtractorAPI(DoodYtExtractor())
+        // 5. MixDrop
+        registerExtractorAPI(MixDrop())
+        // 6. StreamTape
+        registerExtractorAPI(StreamTape())
+        // 7. Voe
+        registerExtractorAPI(Voe())
+        // 8. VidHide
+        registerExtractorAPI(VidHidePro())
+        // 9. OkRu
+        registerExtractorAPI(OkRu())
+        // 10. StreamSB
+        registerExtractorAPI(StreamSB())
     }
 }
