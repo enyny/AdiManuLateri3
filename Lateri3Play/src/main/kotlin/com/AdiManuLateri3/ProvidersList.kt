81b4b098bdfc711ae823011d90ee15d218ec48e5
michat88 <gokilmichat@gmail.com>
2025-12-03 13:23:38 +0900

    Update ProvidersList.kt

--- Lateri3Play/src/main/kotlin/com/AdiManuLateri3/ProvidersList.kt
+++ Lateri3Play/src/main/kotlin/com/AdiManuLateri3/ProvidersList.kt
@@ -3,7 +3,6 @@ package com.AdiManuLateri3
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.utils.ExtractorLink
 
-// Import fungsi dari Extractor yang akan kita buat di file berikutnya
 import com.AdiManuLateri3.Lateri3PlayExtractor.invokeUhdmovies
 import com.AdiManuLateri3.Lateri3PlayExtractor.invokeMultimovies
 import com.AdiManuLateri3.Lateri3PlayExtractor.invokeNinetv
@@ -27,52 +26,34 @@ data class Provider(
 
 fun buildProviders(): List<Provider> {
     return listOf(
-        // 1. UHDMovies (High Quality - GDrive/Index)
         Provider("uhdmovies", "UHD Movies") { res, sc, cb ->
             if (!res.isAnime) invokeUhdmovies(res.title, res.year, res.season, res.episode, sc, cb)
         },
-
-        // 2. MultiMovies (Stable Scraping)
         Provider("multimovies", "MultiMovies") { res, sc, cb ->
             if (!res.isAnime) invokeMultimovies(res.title, res.season, res.episode, sc, cb)
         },
-
-        // 3. NineTV (Simple Iframe)
         Provider("ninetv", "NineTV") { res, sc, cb ->
             if (!res.isAnime) invokeNinetv(res.id, res.season, res.episode, sc, cb)
         },
-
-        // 4. RidoMovies (Good for Movies)
         Provider("ridomovies", "RidoMovies") { res, sc, cb ->
             if (!res.isAnime) invokeRidomovies(res.id, res.imdbId, res.season, res.episode, sc, cb)
         },
-
-        // 5. ZoeChip (Popular)
         Provider("zoechip", "ZoeChip") { res, sc, cb ->
-            if (!res.isAnime) invokeZoechip(res.title, res.year, res.season, res.episode, cb)
+            if (!res.isAnime) invokeZoechip(res.title, res.year, res.season, res.episode, sc, cb)
         },
-
-        // 6. Nepu (AJAX based)
         Provider("nepu", "Nepu") { res, sc, cb ->
             if (!res.isAnime) invokeNepu(res.title, res.year, res.season, res.episode, cb)
         },
-
-        // 7. PlayDesi (Regional/General)
         Provider("playdesi", "PlayDesi") { res, sc, cb ->
             if (!res.isAnime) invokePlaydesi(res.title, res.season, res.episode, sc, cb)
         },
-
-        // 8. Moflix (Aggregator)
         Provider("moflix", "Moflix") { res, sc, cb ->
             if (!res.isAnime) invokeMoflix(res.id, res.season, res.episode, cb)
         },
-
-        // 9. Vidsrc (Basic Backup)
         Provider("vidsrc", "VidSrc") { res, sc, cb ->
-            if (!res.isAnime) invokeVidsrc(res.id, res.season, res.episode, cb)
+            // UPDATE: Menambahkan parameter subtitleCallback (sc) agar sesuai definisi di Extractor
+            if (!res.isAnime) invokeVidsrc(res.id, res.season, res.episode, sc, cb)
         },
-
-        // 10. WatchSoMuch (Subs/Direct Links)
         Provider("watchsomuch", "WatchSoMuch") { res, sc, cb ->
             if (!res.isAnime) invokeWatchsomuch(res.imdbId, res.season, res.episode, sc)
         }
