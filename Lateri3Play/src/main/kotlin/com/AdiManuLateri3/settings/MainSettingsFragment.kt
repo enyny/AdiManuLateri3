8d6ba895bfb264b9784fe07b973557287ed1bce4
michat88 <gokilmichat@gmail.com>
2025-12-03 14:36:36 +0900

    Update MainSettingsFragment.kt

--- Lateri3Play/src/main/kotlin/com/AdiManuLateri3/settings/MainSettingsFragment.kt
+++ Lateri3Play/src/main/kotlin/com/AdiManuLateri3/settings/MainSettingsFragment.kt
@@ -17,99 +17,74 @@ class MainSettingsFragment(
 
     private val res = plugin.resources ?: throw Exception("Unable to access plugin resources")
 
-    // FIX: Menggunakan string literal langsung untuk package name
-    private fun getResId(name: String, type: String): Int {
-        val packageName = "com.AdiManuLateri3"
-        return res.getIdentifier(name, type, packageName)
-    }
-
-    private fun getDrawable(name: String): Drawable? {
-        val id = getResId(name, "drawable")
-        return if (id != 0) res.getDrawable(id, null) else null
+    private fun getDrawable(name: String): Drawable {
+        val id = res.getIdentifier(name, "drawable", BuildConfig.LIBRARY_PACKAGE_NAME)
+        return res.getDrawable(id, null) ?: throw Exception("Drawable $name not found")
     }
 
     private fun <T : View> View.findView(name: String): T {
-        val id = getResId(name, "id")
+        val id = res.getIdentifier(name, "id", BuildConfig.LIBRARY_PACKAGE_NAME)
         if (id == 0) throw Exception("View ID $name not found.")
         return this.findViewById(id)
     }
 
-    private fun getLayout(name: String, inflater: LayoutInflater, container: ViewGroup?): View {
-        val id = getResId(name, "layout")
-        return inflater.inflate(id, container, false)
+    private fun View.makeTvCompatible() {
+        val outlineId = res.getIdentifier("outline", "drawable", BuildConfig.LIBRARY_PACKAGE_NAME)
+        this.background = res.getDrawable(outlineId, null)
     }
 
-    private fun View.makeTvCompatible() {
-        val outlineId = getResId("outline", "drawable")
-        if (outlineId != 0) {
-            this.background = res.getDrawable(outlineId, null)
-            this.isFocusable = true
-            this.isClickable = true
-        }
+    private fun getLayout(name: String, inflater: LayoutInflater, container: ViewGroup?): View {
+        val id = res.getIdentifier(name, "layout", BuildConfig.LIBRARY_PACKAGE_NAME)
+        val layout = res.getLayout(id)
+        return inflater.inflate(layout, container, false)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Menggunakan layout yang nanti akan kita modifikasi (fragment_main_settings)
         val view = getLayout("fragment_main_settings", inflater, container)
 
-        val loginCard: ImageView = view.findView("loginCard")
-        val featureCard: ImageView = view.findView("featureCard")
-        val toggleproviders: ImageView = view.findView("toggleproviders") 
-        val languagechange: ImageView = view.findView("languageCard") 
+        // Hanya mengambil view untuk Sources dan Save (Sesuai request gambar)
+        val toggleproviders: ImageView = view.findView("toggleproviders")
         val saveIcon: ImageView = view.findView("saveIcon")
 
-        loginCard.setImageDrawable(getDrawable("settings_icon"))
-        languagechange.setImageDrawable(getDrawable("settings_icon"))
-        featureCard.setImageDrawable(getDrawable("settings_icon"))
+        // Set Icon
         toggleproviders.setImageDrawable(getDrawable("settings_icon"))
         saveIcon.setImageDrawable(getDrawable("save_icon"))
 
-        loginCard.makeTvCompatible()
-        featureCard.makeTvCompatible()
+        // TV Support
         toggleproviders.makeTvCompatible()
-        languagechange.makeTvCompatible()
         saveIcon.makeTvCompatible()
 
-        // Sembunyikan menu yang tidak terpakai (Login & Extension Toggle)
-        (loginCard.parent.parent as? View)?.visibility = View.GONE
-        (featureCard.parent.parent as? View)?.visibility = View.GONE
-
-        // Tombol Pilih Source
+        // Listener untuk membuka menu Enable/Disable Sources
         toggleproviders.setOnClickListener {
             val providersFragment = ProvidersFragment(plugin, sharedPref)
             providersFragment.show(
-                activity?.supportFragmentManager ?: return@setOnClickListener,
+                activity?.supportFragmentManager ?: throw Exception("No FragmentManager"),
                 "fragment_toggle_providers"
             )
         }
 
-        // Tombol Ganti Bahasa
-        languagechange.setOnClickListener {
-            val langFragment = LanguageSelectFragment(plugin, sharedPref)
-            langFragment.show(
-                activity?.supportFragmentManager ?: return@setOnClickListener,
-                "fragment_language_list"
-            )
-        }
-
-        // Tombol Simpan & Restart
+        // Listener untuk Save & Restart
         saveIcon.setOnClickListener {
             val context = this.context ?: return@setOnClickListener
+
             AlertDialog.Builder(context)
-                .setTitle("Restart Aplikasi")
-                .setMessage("Apakah Anda ingin me-restart aplikasi untuk menerapkan perubahan?")
-                .setPositiveButton("Ya") { _, _ ->
+                .setTitle("Save & Reload")
+                .setMessage("Changes have been saved. Do you want to restart the app to apply them?")
+                .setPositiveButton("Yes") { _, _ ->
                     dismiss()
                     restartApp()
                 }
-                .setNegativeButton("Tidak", null)
+                .setNegativeButton("No", null)
                 .show()
         }
-
         return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {}
 
     private fun restartApp() {
         val context = requireContext().applicationContext
