1c6741b09c0b25a48b2b836683c2ce37f6d3aad1
michat88 <gokilmichat@gmail.com>
2025-12-25 00:02:30 +0900

    Update Extractors.kt

--- Lk21/src/main/kotlin/com/AdiManu/Extractors.kt
+++ Lk21/src/main/kotlin/com/AdiManu/Extractors.kt
@@ -6,8 +6,7 @@ import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.M3u8Helper
 import com.lagradost.cloudstream3.utils.Qualities
-// PERBAIKAN: Import helper baru untuk membuat link extractor
-import com.lagradost.cloudstream3.utils.newExtractorLink 
+import com.lagradost.cloudstream3.utils.newExtractorLink
 import org.json.JSONObject
 
 open class Hownetwork : ExtractorApi() {
@@ -31,7 +30,7 @@ open class Hownetwork : ExtractorApi() {
         val json = JSONObject(response)
         val file = json.optString("file")
         
-        // Menggunakan url asli sebagai referer agar M3U8 valid
+        // Gunakan url iframe sebagai referer untuk validasi M3U8
         M3u8Helper.generateM3u8(this.name, file, url).forEach(callback)
     }
 }
@@ -54,14 +53,15 @@ class HydraxMirror : ExtractorApi() {
     override val mainUrl = "https://short.icu"
     override val requiresReferer = true
     override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
-        // PERBAIKAN: Menggunakan newExtractorLink agar build sukses
+        // PERBAIKAN: Menggunakan urutan posisi (tanpa nama) agar build tidak error
+        // Urutan: Source, Name, Url, Referer, Quality
         callback.invoke(
             newExtractorLink(
-                name = this.name,
-                source = this.name,
-                url = url,
-                referer = referer ?: "",
-                quality = Qualities.Unknown.value
+                this.name, 
+                this.name, 
+                url, 
+                referer ?: "", 
+                Qualities.Unknown.value
             )
         )
     }
