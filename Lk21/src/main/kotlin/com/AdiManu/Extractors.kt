246b958a24a82c3917e4bf95c2af27e66a92bc8f
michat88 <gokilmichat@gmail.com>
2025-12-26 19:02:15 +0900

    Update Extractors.kt

--- Lk21/src/main/kotlin/com/AdiManu/Extractors.kt
+++ Lk21/src/main/kotlin/com/AdiManu/Extractors.kt
@@ -1,13 +1,24 @@
 package com.AdiManu
 
+import com.lagradost.api.Log
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.extractors.Filesim
+import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.INFER_TYPE
 import com.lagradost.cloudstream3.utils.M3u8Helper
-import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.getQualityFromName
 import com.lagradost.cloudstream3.utils.newExtractorLink
 import org.json.JSONObject
+
+
+class Co4nxtrl : Filesim() {
+    override val mainUrl = "https://co4nxtrl.com"
+    override val name = "Co4nxtrl"
+    override val requiresReferer = true
+}
 
 open class Hownetwork : ExtractorApi() {
     override val name = "Hownetwork"
@@ -23,46 +34,41 @@ open class Hownetwork : ExtractorApi() {
         val id = url.substringAfter("id=")
         val response = app.post(
                 "$mainUrl/api.php?id=$id",
-                data = mapOf("r" to "", "d" to mainUrl),
+                data = mapOf(
+                        "r" to "",
+                        "d" to mainUrl,
+                ),
                 referer = url,
-                headers = mapOf("X-Requested-With" to "XMLHttpRequest")
+                headers = mapOf(
+                        "X-Requested-With" to "XMLHttpRequest"
+                )
         ).text
         val json = JSONObject(response)
         val file = json.optString("file")
-        
-        // Gunakan url iframe sebagai referer untuk validasi M3U8
-        M3u8Helper.generateM3u8(this.name, file, url).forEach(callback)
+        Log.d("Phisher", file)
+            M3u8Helper.generateM3u8(
+                this.name,
+                file,
+                file
+            ).forEach(callback)
     }
+}
+
+class Furher : Filesim() {
+    override val name = "Furher"
+    override var mainUrl = "https://furher.in"
 }
 
 class Cloudhownetwork : Hownetwork() {
     override var mainUrl = "https://cloud.hownetwork.xyz"
 }
 
-class VidHideClone : ExtractorApi() {
-    override val name = "VidHide Mirror"
-    override val mainUrl = "https://f16px.com"
-    override val requiresReferer = true
-    override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
-        com.lagradost.cloudstream3.extractors.VidHidePro6().getUrl(url, referer, subtitleCallback, callback)
-    }
+class Furher2 : Filesim() {
+    override val name = "Furher 2"
+    override var mainUrl = "723qrh1p.fun"
 }
 
-class HydraxMirror : ExtractorApi() {
-    override val name = "Hydrax"
-    override val mainUrl = "https://short.icu"
-    override val requiresReferer = true
-    override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
-        // PERBAIKAN: Menggunakan urutan posisi (tanpa nama) agar build tidak error
-        // Urutan: Source, Name, Url, Referer, Quality
-        callback.invoke(
-            newExtractorLink(
-                this.name, 
-                this.name, 
-                url, 
-                referer ?: "", 
-                Qualities.Unknown.value
-            )
-        )
-    }
+class Turbovidhls : Filesim() {
+    override val name = "Turbovidhls"
+    override var mainUrl = "https://turbovidhls.com"
 }
