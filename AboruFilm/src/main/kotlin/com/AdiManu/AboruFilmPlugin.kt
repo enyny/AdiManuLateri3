fd65f590975e2cd867af732faeae5322eaae6ec5
michat88 <gokilmichat@gmail.com>
2025-12-30 16:57:45 +0900

    Update AboruFilmPlugin.kt

--- AboruFilm/src/main/kotlin/com/AdiManu/AboruFilmPlugin.kt
+++ AboruFilm/src/main/kotlin/com/AdiManu/AboruFilmPlugin.kt
@@ -1,5 +1,4 @@
 package com.AdiManu
-
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
@@ -7,9 +6,6 @@ import android.content.Context
 @CloudstreamPlugin
 class AboruFilmPlugin: Plugin() {
     override fun load(context: Context) {
-        // Kita menginisialisasi API utama (AboruFilm).
-        // Tidak perlu passing 'sharedPreferences' atau 'settings' karena token sudah di-hardcode.
-        val api = AboruFilm() 
-        registerMainAPI(api)
+        registerMainAPI(AboruFilm())
     }
 }
