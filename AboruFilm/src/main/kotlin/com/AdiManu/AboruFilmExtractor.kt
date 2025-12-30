5e43c5caf329ecf8a2934dafcb615dcf45b8266e
michat88 <gokilmichat@gmail.com>
2025-12-30 17:02:52 +0900

    Update AboruFilmExtractor.kt

--- AboruFilm/src/main/kotlin/com/AdiManu/AboruFilmExtractor.kt
+++ AboruFilm/src/main/kotlin/com/AdiManu/AboruFilmExtractor.kt
@@ -7,56 +7,52 @@ import org.jsoup.Jsoup
 
 object AboruFilmExtractor : AboruFilm() {
 
-    private val videoHeaders = mapOf(
-        "User-Agent" to "okhttp/3.12.1",
-        "Referer" to "https://www.febbox.com/",
-        "Accept" to "*/*"
-    )
+    private val vHeaders = mapOf("User-Agent" to "okhttp/3.12.1", "Referer" to "https://www.febbox.com/")
 
-    suspend fun invokeInternalSource(id: Int?, type: Int?, season: Int?, episode: Int?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
-        val query = if (type == 1) 
-            """{"childmode":"0","app_version":"11.7","appid":"$appId","module":"Movie_downloadurl_v3","mid":"$id","lang":"en","expired_date":"${getExpiryDate()}","platform":"android","uid":"$HARDCODED_TOKEN"}"""
-        else 
-            """{"childmode":"0","app_version":"11.7","appid":"$appId","module":"TV_downloadurl_v3","episode":"$episode","season":"$season","tid":"$id","lang":"en","expired_date":"${getExpiryDate()}","platform":"android","uid":"$HARDCODED_TOKEN"}"""
-
+    suspend fun invokeInternalSource(id: Int?, type: Int?, s: Int?, e: Int?, subCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit) {
+        val q = if (type == 1) """{"module":"Movie_downloadurl_v3","mid":"$id","uid":"$HARDCODED_TOKEN","platform":"android","appid":"$appId"}"""
+                else """{"module":"TV_downloadurl_v3","tid":"$id","season":"$s","episode":"$e","uid":"$HARDCODED_TOKEN","platform":"android","appid":"$appId"}"""
+        
         try {
-            val response = queryApiParsed<LinkDataProp>(query)
-            response.data?.list?.forEach { link ->
-                val path = link.path ?: return@forEach
-                callback.invoke(newExtractorLink("Aboru Internal", "Aboru Internal ${link.quality ?: ""}", path, INFER_TYPE) {
-                    this.quality = getQualityFromName(link.quality); this.headers = videoHeaders
-                })
+            val res = queryApiParsed<LinkDataProp>(q)
+            res.data?.list?.forEach { link ->
+                callback.invoke(newExtractorLink("Aboru Internal", "Internal ${link.quality ?: ""}", link.path ?: return@forEach, INFER_TYPE) { this.headers = vHeaders })
             }
-        } catch (e: Exception) { e.printStackTrace() }
+        } catch (err: Exception) { }
     }
 
-    suspend fun invokeExternalSource(mediaId: Int?, type: Int?, season: Int?, episode: Int?, callback: (ExtractorLink) -> Unit) {
+    suspend fun invokeExternalSource(mid: Int?, type: Int?, s: Int?, e: Int?, callback: (ExtractorLink) -> Unit) {
         try {
-            val shareRes = app.get("$thirdAPI/mbp/to_share_page?box_type=$type&mid=$mediaId&json=1").parsedSafe<ExternalResponse>()?.data ?: return
-            val shareKey = shareRes.link ?: return
-            val fileList = app.get("$thirdAPI/file/file_share_list?share_key=$shareKey").parsedSafe<ExternalResponse>()?.data?.file_list ?: return
+            val share = app.get("$thirdAPI/mbp/to_share_page?box_type=$type&mid=$mid&json=1").parsedSafe<ExternalResponse>()?.data?.link ?: return
+            val files = app.get("$thirdAPI/file/file_share_list?share_key=$share").parsedSafe<ExternalResponse>()?.data?.file_list ?: return
             
-            fileList.forEach { file ->
-                val player = app.get("$thirdAPI/console/video_quality_list?fid=${file.fid}&share_key=$shareKey", headers = mapOf("Cookie" to "ui=$HARDCODED_TOKEN")).text
-                val html = JSONObject(player).optString("html")
+            files.amap { file ->
+                val p = app.get("$thirdAPI/console/video_quality_list?fid=${file.fid}&share_key=$share", headers = mapOf("Cookie" to "ui=$HARDCODED_TOKEN")).text
+                val html = JSONObject(p).optString("html")
                 Jsoup.parse(html).select("div.file_quality").forEach { el ->
                     val url = el.attr("data-url")
-                    if (url.isNotEmpty()) {
-                        callback.invoke(newExtractorLink("Aboru External", "Server External ${el.attr("data-quality")}", url, INFER_TYPE) {
-                            this.headers = videoHeaders
-                        })
-                    }
+                    if (url.isNotEmpty()) callback.invoke(newExtractorLink("Aboru External", "Server ${el.attr("data-quality")}", url, INFER_TYPE) { this.headers = vHeaders })
                 }
             }
-        } catch (e: Exception) { }
+        } catch (err: Exception) { }
     }
 
-    suspend fun invokeExternalM3u8Source(mediaId: Int?, type: Int?, s: Int?, e: Int?, callback: (ExtractorLink) -> Unit) { /* Logika M3U8 mirip dengan External Source */ }
+    suspend fun invokeWatchsomuch(imdb: String?, s: Int?, e: Int?, subCallback: (SubtitleFile) -> Unit) {
+        try {
+            val id = imdb?.removePrefix("tt")
+            val res = app.post("$watchSomuchAPI/Watch/ajMovieTorrents.aspx", data = mapOf("mid" to "$id", "wsk" to "30fb68aa-1c71-4b8c-b5d4-4ca9222cfb45")).parsedSafe<WatchsomuchResponses>()
+            val tid = res?.movie?.torrents?.find { if (s == null) true else it.season == s && it.episode == e }?.id ?: return
+            val subUrl = "$watchSomuchAPI/Watch/ajMovieSubtitles.aspx?mid=$id&tid=$tid"
+            app.get(subUrl).parsedSafe<WatchsomuchSubResponses>()?.subtitles?.forEach { sub ->
+                subCallback.invoke(newSubtitleFile(sub.label ?: "English", sub.url ?: return@forEach))
+            }
+        } catch (err: Exception) { }
+    }
 
-    suspend fun invokeOpenSubs(imdbId: String?, s: Int?, e: Int?, callback: (SubtitleFile) -> Unit) {
-        val slug = if (s == null) "movie/$imdbId" else "series/$imdbId:$s:$e"
+    suspend fun invokeOpenSubs(imdb: String?, s: Int?, e: Int?, subCallback: (SubtitleFile) -> Unit) {
+        val slug = if (s == null) "movie/$imdb" else "series/$imdb:$s:$e"
         app.get("$openSubAPI/subtitles/$slug.json").parsedSafe<OsResult>()?.subtitles?.forEach {
-            callback.invoke(newSubtitleFile(it.lang ?: "English", it.url ?: return@forEach))
+            subCallback.invoke(newSubtitleFile(it.lang ?: "English", it.url ?: return@forEach))
         }
     }
 }
