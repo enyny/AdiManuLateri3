623582fd67695d21b9aa80f7f10fc203e80c2add
michat88 <gokilmichat@gmail.com>
2025-12-30 17:37:03 +0900

    Update AboruFilmPlugin.kt

--- AboruFilm/src/main/kotlin/com/AdiManu/AboruFilmPlugin.kt
+++ AboruFilm/src/main/kotlin/com/AdiManu/AboruFilmPlugin.kt
@@ -7,9 +7,9 @@ import android.content.Context
 @CloudstreamPlugin
 class AboruFilmPlugin: Plugin() {
     override fun load(context: Context) {
-        // Mendaftarkan API utama AboruFilm.
-        // Berbeda dengan Superstream asli, kita tidak memerlukan sharedPreferences 
-        // karena token sudah di-hardcode di dalam kelas AboruFilm.
+        [span_3](start_span)// Mendaftarkan API utama AboruFilm ke aplikasi Cloudstream.[span_3](end_span)
+        // Berbeda dengan file asli Superstream, kita memanggil AboruFilm() secara langsung
+        [span_4](start_span)// karena semua rahasia (token & sertifikat) sudah ada di dalam kelas tersebut.[span_4](end_span)
         val api = AboruFilm() 
         registerMainAPI(api)
     }
