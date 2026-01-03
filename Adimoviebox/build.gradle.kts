dd971a911aa958b0ba0bc24b0d99c5dc6fc53a37
michat88 <gokilmichat@gmail.com>
2026-01-03 23:32:35 +0900

    Update build.gradle.kts

--- Adimoviebox/build.gradle.kts
+++ Adimoviebox/build.gradle.kts
@@ -1,14 +1,12 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.gradle.kotlin.dsl.configure
 
-version = 2 // Versi dinaikkan karena ada perubahan besar
+version = 2 // Versi diperbarui karena perubahan besar pada API
 
 cloudstream {
     description = "Nonton sampe biji mata kaluar"
     language    = "id"
     authors = listOf("AdiManuLateri3")
     status = 1
-    [span_1](start_span)// Mendukung semua tipe dari Moviebox[span_1](end_span)
     tvTypes = listOf("Movie", "TvSeries", "Anime", "AsianDrama")
     iconUrl = "https://raw.githubusercontent.com/michat88/Zaneta/refs/heads/main/Icons/adi.png"
     isCrossPlatform = true
