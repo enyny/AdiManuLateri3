5afc3af6aad0e6db51805ae66021f174ce7c3c49
michat88 <gokilmichat@gmail.com>
2026-01-05 17:41:50 +0900

    Update build.gradle.kts

--- Adimoviebox/build.gradle.kts
+++ Adimoviebox/build.gradle.kts
@@ -1,6 +1,6 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
-version = 5 // Versi diperbarui karena perubahan besar pada API
+version = 6 // Versi diperbarui karena perubahan besar pada API
 
 cloudstream {
     description = "Nonton sampe biji mata kaluar"
