da6e8a6c2f6a017eb7acd03c0479dae729a8370b
michat88 <gokilmichat@gmail.com>
2026-02-09 20:46:54 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,8 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/michat88/AdiManuLateri3")
-        authors = listOf("AldryManu")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/phisher98/cloudstream-extensions-phisher")
+        authors = listOf("Phisher98")
     }
 
     android {
